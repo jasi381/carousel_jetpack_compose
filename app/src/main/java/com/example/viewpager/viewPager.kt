387020a01b8b5782ca39c854246975cc5ff45443
jasi381 <sjasmeet438@gmail.com>
2023-02-06 14:29:39 +0530

    ..

--- app/src/main/java/com/example/viewpager/viewPager.kt
+++ app/src/main/java/com/example/viewpager/viewPager.kt
@@ -9,9 +9,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -19,43 +17,32 @@ import com.google.accompanist.pager.*
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
- fun Sample() {
-    Scaffold(
-        backgroundColor = Color.Cyan,
-        topBar = {
-            TopAppBar(
-                title = { Text(stringResource(R.string.app_name)) },
-                backgroundColor = Color.DarkGray,
-            )
-        },
-        modifier = Modifier.fillMaxSize()
-    ) { padding ->
-        Column(
-            Modifier
-                .fillMaxSize()
-                .padding(top = 150.dp)) {
-            val pagerState = rememberPagerState()
-            HorizontalPager(
-                count = 10,
-                state = pagerState,
-                contentPadding = PaddingValues(horizontal = 1.dp),
+fun Sample() {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 150.dp)) {
+        val pagerState = rememberPagerState()
+        HorizontalPager(
+            count = 10,
+            state = pagerState,
+            contentPadding = PaddingValues(horizontal = 1.dp),
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            PagerSampleItem(
                 modifier = Modifier
-                    .fillMaxWidth(),
-            ) {
-                PagerSampleItem(
-                    modifier = Modifier
-                        .height(290.dp).fillMaxWidth()
-                        .aspectRatio(1f)
-                )
-            }
-
-            HorizontalPagerIndicator(
-                pagerState = pagerState,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(17.dp),
+                    .height(290.dp).fillMaxWidth()
+                    .aspectRatio(1f)
             )
         }
+
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(17.dp),
+        )
     }
 }
 
@@ -91,7 +78,7 @@ fun rememberRandomSampleImageUrl(
     width: Int = 250,
     height: Int = width,
 
-): String = remember { randomSampleImageUrl(seed, width, height) }
+    ): String = remember { randomSampleImageUrl(seed, width, height) }
 
 @Preview
 @Composable
